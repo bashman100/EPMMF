38300efa87a6cd089ee847d90e3170eab610d57b
Sebastian Mohr <bashman2000@ymail.com>
2017-03-01 10:40:57 -0700

    Fixed allowed hosts v1.41

--- EPMMF/settings.py
+++ EPMMF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '()$wup4y6fospgaa&av+ij+hz^@-a^hf(5d*+^jf_$k)n&g*_k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,34 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bands',
 ]
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
 
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+     'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
