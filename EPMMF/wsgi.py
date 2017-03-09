64f58975139ac2dbefd54c56a1a5c4f90a7efcf3
Sebastian Mohr <bashman2000@ymail.com>
2017-03-09 08:32:23 -0700

    roll back and prepare for heroku push v2.2

--- EPMMF/wsgi.py
+++ EPMMF/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-import sys
-sys.path.append('/opt/bitnami/apps/django/django_projects/EPMMF')
-os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/apps/django/django_projects/EPMMF/egg_cache")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EPMMF.settings")
-from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
