38ecc095d188d1420e418394f4c77bc3052f4782
Sebastian Mohr <bashman2000@ymail.com>
2017-04-12 17:51:59 -0600

    Fixed the logo to be more compatable v2.2.5

--- EPMMF/settings.py
+++ EPMMF/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '()$wup4y6fospgaa&av+ij+hz^@-a^hf(5d*+^jf_$k)n&g*_k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
